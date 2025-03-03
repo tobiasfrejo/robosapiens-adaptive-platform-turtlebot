33629549f3c73f8b0dd7ebef7da68a0cf0b1bf99
Tobias Frejo <tobias@frejo.dk>
2025-03-03 14:18:26 +0100

    general read/write message

--- rv-tools-dev/rv_tools/knowledge/knowledge_rv.py
+++ rv-tools-dev/rv_tools/knowledge/knowledge_rv.py
@@ -3,10 +3,14 @@ import re
 
 from rpio.clientLibraries.rpclpy.node import Node, KnowledgeManager, CommunicationManager
 
-def _ensure_publish_key_exist(node, key):
+def _ensure_publish_key_exist(node, key, make_trustworthiness_checker_safe:bool=True):
     # You should not have to declare these in the config. Or should you??
 
-    event_key = _trustworthiness_checker_safe_key('k_' + key)
+    if make_trustworthiness_checker_safe:
+        event_key = _trustworthiness_checker_safe_key('k_' + key)
+    else:
+        event_key = key
+
     # TODO: can we do this cleaner?
     if event_key not in node.communication_manager.mqtt_publish_topics_map:
         node.communication_manager.mqtt_publish_topics_map[event_key] = event_key
@@ -19,10 +23,12 @@ def write(node:Node, key:str, value = True):
     event_key = _ensure_publish_key_exist(node, key)
     node.knowledge.write(key, value)
     node.publish_event(event_key, message=json.dumps({"Str": "write"}))
+    node.publish_event(_ensure_publish_key_exist(node, 'any_knowledge_write', False), message=json.dumps({'Str':key}))
 
 def read(node:Node, key, queueSize = 1):
     event_key = _ensure_publish_key_exist(node, key)
     data = node.knowledge.read(key, queueSize)
     node.publish_event(event_key, message=json.dumps({"Str": "read"}))
+    node.publish_event(_ensure_publish_key_exist(node, 'any_knowledge_read', False), message=json.dumps({'Str':key}))
 
     return data
