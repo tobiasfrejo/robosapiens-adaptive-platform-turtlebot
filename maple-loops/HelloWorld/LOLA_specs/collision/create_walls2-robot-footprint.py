62e08bcf7b7dfd7e08361874d13b0a29f06a81fc
Tobias Frejo <tobias@frejo.dk>
2025-03-27 12:59:14 +0000

    Draw turtle map

---
+++ maple-loops/HelloWorld/LOLA_specs/collision/create_walls2-robot-footprint.py
@@ -0,0 +1,157 @@
+from warnings import warn
+import numpy as np
+from itertools import chain
+# Scaled to keep precision as ints
+
+corners = np.array([
+    (-2.8868,  0.0),
+    (-1.7248, -2.0125),
+    (-1.4031, -2.0125),
+    (-1.1216, -2.5),
+    ( 1.1216, -2.5),
+    ( 1.4031, -2.0125),
+    ( 1.7248, -2.0125),
+    ( 2.616,  -0.4689),
+    ( 2.3453,  0.0),
+    ( 2.616,   0.4689),
+    ( 1.7248,  2.0125),
+    ( 1.4031,  2.0125),
+    ( 1.1216,  2.5),
+    (-1.1216,  2.5),
+    (-1.4031,  2.0125),
+    (-1.7248,  2.0125),
+])
+
+# obstacle1 = np.array([
+#     (-3,  1),
+#     (-1,  0),
+#     ( 0, -3),
+#     ( 0,  1)
+# ])
+
+def connect_polygon(corners):
+    walls = []
+    for i in range(len(corners)):
+        j = (i - 1) % len(corners)
+        walls.append((corners[i], corners[j]))
+        warn(f'({i}, {j}): {corners[i]}, {corners[j]}')
+    walls = np.array(walls)
+    return walls
+
+walls = np.concat((
+    connect_polygon(corners),
+    # connect_polygon(obstacle1)
+))
+warn(str(len(walls)))
+
+# (Cx, Cy, R)
+pillars = np.array([
+    (-1.1,  -1.1,  0.15),
+    (-1.1,   0.0,  0.15),
+    (-1.1,   1.1,  0.15),
+    ( 0.0,  -1.1,  0.15),
+    ( 0.0,   0.0,  0.15),
+    ( 0.0,   1.1,  0.15),
+    ( 1.1,  -1.1,  0.15),
+    ( 1.1,   0.0,  0.15),
+    ( 1.1,   1.1,  0.15),
+])
+
+# Collision if distance to center is smaller than the radius.
+#  (x-a)² + (y-b)² <= r²
+"""
+Could potentially be optimized to avoid extra multiplication operations if outside the circumscribed square:
+(\
+    PosX <= {cxpr} && \
+    {cxmr} <= PosX && \
+    PosY <= {cypr} && \
+    {cymr} <= PosY
+) && ...
+"""
+circle_collision_expression = '''\
+(\
+   (((PosX - {cx}) * (PosX - {cx})) \
+  + ((PosY - {cy}) * (PosY - {cy}))) \
+    <= ({r} * {r})\
+)'''
+
+"""
+https://wrfranklin.org/Research/Short_Notes/pnpoly.html
+
+(Ay > Py  !=  By > Py)
+&&  (Px  <  (Bx-Ax) * (Py-Ay) / (By-Ay) + Ax)
+"""
+expression = """\
+if !(\
+ !({ay} <= PosY) == !({by} <= PosY)\
+) && !(\
+ (({a}) * ((PosY) - ({ay})) + ({ax1})) <= (PosX)) \
+then 1 \
+else 0 \
+"""
+
+
+expressions = []
+for A, B in walls:
+    Ax, Ay = A
+    Bx, By = B
+
+    if Ay == By:
+        continue
+
+    expressions.append(expression.format_map({
+        'a':(Bx-Ax)/(By-Ay),
+        'ax1': Ax,
+        'ay': Ay,
+        'ay1': Ay,
+        'bx': Bx,
+        'by': By
+    }))
+
+circle_expressions = []
+for cx,cy,r in pillars:
+    circle_expressions.append(circle_collision_expression.format_map({
+        'cx': cx,
+        'cy': cy,
+        'r': r
+    }))
+
+streams = []
+declarations = []
+
+for n,expr in enumerate(expressions):
+    wx = 'w'+str(n)
+    streams.append(wx)
+    declarations.append(f'{wx} = {expr}')
+
+circles = []
+for n,expr in enumerate(circle_expressions):
+    cx = 'c'+str(n)
+    circles.append(cx)
+    declarations.append(f'{cx} = {expr}')
+
+output = """\
+in Pos
+out PosX
+out PosY
+out inside
+out seenwalls
+out collision
+"""
+for s in chain(streams, circles):
+    output+= f'out {s}\n'
+output += """\
+PosX = List.get(Pos, 0)
+PosY = List.get(Pos, 1)
+"""
+for d in declarations:
+    output+= f'{d}\n'
+
+output += f'seenwalls = ({" + ".join(streams)})\n'
+output += 'inside = (seenwalls % 2) == 1\n'
+if len(circles) == 0:
+    output += 'collision = !inside'
+else:
+    output += f'collision = !inside && !({ " || ".join(circles) })\n'
+
+print(output)
