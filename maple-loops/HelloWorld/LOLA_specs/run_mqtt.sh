945eeffba1af774bf1750ecd290448e1d638b96c
Tobias Frejo <tobias@frejo.dk>
2025-04-09 08:54:50 +0000

    rename mqtt run script

---
+++ maple-loops/HelloWorld/LOLA_specs/run_mqtt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SPECFILE="$(realpath "$1")"
+SPECBASE="$(basename "$SPECFILE")"
+HOSTDIR="$(dirname "$SPECFILE")"
+
+IN_TOPICS="$(awk -F'[: ]+' '/^in / {print $2}' $SPECFILE | tr '\n' ' ' | sed 's/ *$//')"
+OUT_TOPICS="$(awk -F'[: ]+' '/^out / {print $2}' $SPECFILE | tr '\n' ' ' | sed 's/ *$//')"
+
+CMD="docker run --network host -it --rm -e RUST_BACKTRACE=full -v $HOSTDIR:/mnt/host_models trustworthiness-checker /mnt/host_models/$SPECBASE --input-mqtt-topics $IN_TOPICS  --output-mqtt-topics $OUT_TOPICS --output-mqtt-topic-prefix lola/$SPECBASE/"
+
+{
+echo "Command: $CMD"
+echo ""
+echo "========     SPECIFICATION      ======="
+echo "Source: $SPECBASE"
+cat "$SPECFILE"
+echo ""
+echo ""
+echo "======== TRUSTWORTHINESS OUTPUT ======="
+$CMD
+}
--- maple-loops/HelloWorld/LOLA_specs/run_mqtt_debug.sh
+++ maple-loops/HelloWorld/LOLA_specs/run_mqtt_debug.sh
@@ -4,15 +4,10 @@ SPECFILE="$(realpath "$1")"
 SPECBASE="$(basename "$SPECFILE")"
 HOSTDIR="$(dirname "$SPECFILE")"
 
-LOGFILE="Logs/${SPECBASE}_$(date +%Y-%m-%d_%H.%M.%S).txt"
-
 IN_TOPICS="$(awk -F'[: ]+' '/^in / {print $2}' $SPECFILE | tr '\n' ' ' | sed 's/ *$//')"
 OUT_TOPICS="$(awk -F'[: ]+' '/^out / {print $2}' $SPECFILE | tr '\n' ' ' | sed 's/ *$//')"
 
-CMD="docker run --network host -it --rm -e RUST_BACKTRACE=full -v $HOSTDIR:/mnt/host_models trustworthiness-checker /mnt/host_models/$SPECBASE --input-mqtt-topics $IN_TOPICS  --output-mqtt-topics $OUT_TOPICS"
-
-echo "Saving output to: $LOGFILE"
-echo ""
+CMD="docker run --network host -it --rm -e RUST_BACKTRACE=full -v $HOSTDIR:/mnt/host_models trustworthiness-checker:debug /mnt/host_models/$SPECBASE --input-mqtt-topics $IN_TOPICS  --output-mqtt-topics $OUT_TOPICS --output-mqtt-topic-prefix lola/$SPECBASE/"
 
 {
 echo "Command: $CMD"
@@ -23,5 +18,5 @@ cat "$SPECFILE"
 echo ""
 echo ""
 echo "======== TRUSTWORTHINESS OUTPUT ======="
-$CMD 2>&1
-} | tee -a "$LOGFILE"
+$CMD
+}
