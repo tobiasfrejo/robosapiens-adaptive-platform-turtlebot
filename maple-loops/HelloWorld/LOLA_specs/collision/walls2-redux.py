f620a74fd13654b94ae377404541a47643fab1f6
Amalie Kaastrup-Hansen <amalie@kaastrup-hansen.dk>
2025-04-23 10:04:58 +0200

    Redux 2 and 3 updated to use dyn_lola and collapse

--- maple-loops/HelloWorld/LOLA_specs/collision/walls2-redux.py
+++ maple-loops/HelloWorld/LOLA_specs/collision/walls2-redux.py
@@ -1,8 +1,8 @@
 from itertools import chain
-from dynamic_walls.lola import LolaSpecification, LolaStream, lola_chain
-from dynamic_walls.turtlebot import tb3_corners, turtle_map, turtle_map_pillars
-from dynamic_walls.geometry import rotate_polygon, connect_polygon, circle_line_overlap, test_points_in_circles
-from dynamic_walls.pnpoly import pnpoly
+from dyn_lola.lola import LolaSpecification, LolaStream, lola_chain, Expression, lnot
+from dyn_lola.shapes import turtlebot
+from dyn_lola.geometry import rotate_polygon, connect_polygon, circle_line_overlap, test_points_in_circles
+from dyn_lola.pnpoly import pnpoly
 
 spec = LolaSpecification()
 
@@ -11,13 +11,13 @@ y = LolaStream('y')
 a = LolaStream('a')
 
 spec.inputs.append(LolaStream('Odometry'))
-spec.add_expression(x, 'List.get(Odometry, 0)')
-spec.add_expression(y, 'List.get(Odometry, 1)')
-spec.add_expression(a, 'List.get(Odometry, 2)')
+spec.add_expression(x, Expression('List.get(Odometry, 0)'), keep_on_prune=True)
+spec.add_expression(y, Expression('List.get(Odometry, 1)'), keep_on_prune=True)
+spec.add_expression(a, Expression('List.get(Odometry, 2)'), keep_on_prune=True)
 
-tb3_exprs, tb3_rotated_corners = rotate_polygon(tb3_corners, (x,y), a)
+tb3_exprs, tb3_rotated_corners = rotate_polygon(turtlebot.tb3_corners, (x,y), a)
 
-map_walls = connect_polygon(turtle_map)
+map_walls = connect_polygon(turtlebot.turtle_map)
 obstacle_wall = connect_polygon([
     (-0.1, 2.1),
     (-0.1, 1.9),
@@ -26,23 +26,31 @@ obstacle_wall = connect_polygon([
 ])
 
 wall_exprs, pnp = pnpoly(tb3_rotated_corners, map_walls + obstacle_wall)
-pillars_expressions, pillar_point_streams, _ = test_points_in_circles(tb3_rotated_corners, turtle_map_pillars)
+pillars_expressions, pillar_point_streams, _ = test_points_in_circles(tb3_rotated_corners, turtlebot.turtle_map_pillars)
 
 for k,v in chain.from_iterable((tb3_exprs.items(), wall_exprs.items(), pillars_expressions.items())):
     spec.add_expression(k,v)
 
 in_map = LolaStream('InMap')
-spec.add_expression(in_map, lola_chain(pnp, '&&'))
+spec.add_expression(in_map, lola_chain(pnp, '&&'), keep_on_prune=True)
+spec.collapse_expression(in_map)
 
 corner_collision_streams = []
 for n,streams in pillar_point_streams.items():
     s = LolaStream(f'Corner{n}Collision')
-    spec.add_expression(s, lola_chain(streams, '||'))
+    spec.add_expression(s, lola_chain(streams, '||'), keep_on_prune=True)
     corner_collision_streams.append(s)
+    spec.collapse_expression(s)
 
 any_pillar_collision = lola_chain(corner_collision_streams, '||')
 
-spec.add_expression(LolaStream('Collision'), f"!{in_map} || ({any_pillar_collision})")
+collision_stream = LolaStream('Collision')
+collision_exp = lola_chain([lnot(in_map),any_pillar_collision], '||')
+spec.add_expression(collision_stream, collision_exp, keep_on_prune=True)
+spec.collapse_expression(collision_stream)
 
-print(spec.get_specification_string())
+spec.prune()
+#spec.write_specification('test2_collapsed.lola')
+
+#print(spec.get_specification_string())
 spec.write_specification('walls2-redux.lola')
--- maple-loops/HelloWorld/LOLA_specs/collision/walls3-redux.py
+++ maple-loops/HelloWorld/LOLA_specs/collision/walls3-redux.py
@@ -1,8 +1,8 @@
 from itertools import chain
-from dynamic_walls.lola import LolaSpecification, LolaStream, lola_chain
-from dynamic_walls.turtlebot import tb3_corners, turtle_map, turtle_map_pillars
-from dynamic_walls.geometry import rotate_polygon, connect_polygon, test_circles_walls_overlaps
-from dynamic_walls.pnpoly import pnpoly
+from dyn_lola.lola import LolaSpecification, LolaStream, lola_chain, Expression
+from dyn_lola.shapes import turtlebot
+from dyn_lola.geometry import rotate_polygon, connect_polygon, test_circles_walls_overlaps
+from dyn_lola.pnpoly import pnpoly
 
 spec = LolaSpecification()
 
@@ -11,11 +11,11 @@ y = LolaStream('y')
 a = LolaStream('a')
 
 spec.inputs.append(LolaStream('Odometry'))
-spec.add_expression(x, 'List.get(Odometry, 0)')
-spec.add_expression(y, 'List.get(Odometry, 1)')
-spec.add_expression(a, 'List.get(Odometry, 2)')
+spec.add_expression(x, Expression('List.get(Odometry, 0)'), keep_on_prune=True)
+spec.add_expression(y, Expression('List.get(Odometry, 1)'), keep_on_prune=True)
+spec.add_expression(a, Expression('List.get(Odometry, 2)'), keep_on_prune=True)
 
-tb3_exprs, tb3_rotated_corners = rotate_polygon(tb3_corners, (x,y), a)
+tb3_exprs, tb3_rotated_corners = rotate_polygon(turtlebot.tb3_corners, (x,y), a)
 
 # map_walls = connect_polygon(turtle_map)
 # obstacle_wall = connect_polygon([
@@ -25,17 +25,17 @@ tb3_exprs, tb3_rotated_corners = rotate_polygon(tb3_corners, (x,y), a)
 #     ( 0.1, 2.1),
 # ])
 
-world_points = turtle_map + [
+world_points = turtlebot.turtle_map + [
     (-0.1, 2.1),
     (-0.1, 1.9),
     ( 0.1, 1.9),
     ( 0.1, 2.1),
-] + list(map(lambda p: p[0], turtle_map_pillars))
+] + list(map(lambda p: p[0], turtlebot.turtle_map_pillars))
 
 tb3_edges = connect_polygon(tb3_rotated_corners)
 
 wall_exprs, pnp = pnpoly(world_points, tb3_edges)
-pillars_expressions, _, pillar_point_streams = test_circles_walls_overlaps(turtle_map_pillars, tb3_edges)
+pillars_expressions, _, pillar_point_streams = test_circles_walls_overlaps(turtlebot.turtle_map_pillars, tb3_edges)
 
 for k,v in chain.from_iterable((tb3_exprs.items(), wall_exprs.items(), pillars_expressions.items())):
     spec.add_expression(k,v)
@@ -46,12 +46,17 @@ spec.add_expression(corner_collision, lola_chain(pnp, '||'))
 pillar_collision_streams = []
 for n,streams in pillar_point_streams.items():
     s = LolaStream(f'Pillar{n}Collision')
-    spec.add_expression(s, lola_chain(streams, '||'))
+    spec.add_expression(s, lola_chain(streams, '||'), keep_on_prune=True)
     pillar_collision_streams.append(s)
+    spec.collapse_expression(s)
 
 any_pillar_collision = lola_chain(pillar_collision_streams, '||')
 
-spec.add_expression(LolaStream('Collision'), f"{corner_collision} || ({any_pillar_collision})")
+collision_stream = LolaStream('Collision')
+collision_exp = lola_chain([corner_collision,any_pillar_collision], '||')
+spec.add_expression(collision_stream, collision_exp, keep_on_prune=True)
+spec.collapse_expression(collision_stream)
+spec.prune()
 
 print(spec.get_specification_string())
 spec.write_specification('walls3-redux.lola')
