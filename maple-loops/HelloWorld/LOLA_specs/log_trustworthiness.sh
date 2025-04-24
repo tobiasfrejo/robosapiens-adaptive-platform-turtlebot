35f86fbd5f6dc1c7b4c2a0c99e3586e9c4577510
Tobias Frejo <tobias@frejo.dk>
2025-04-24 08:00:31 +0000

    Fix timestamp pipe

--- maple-loops/HelloWorld/LOLA_specs/log_trustworthiness.sh
+++ maple-loops/HelloWorld/LOLA_specs/log_trustworthiness.sh
@@ -25,4 +25,4 @@ echo ""
 echo ""
 echo "======== TRUSTWORTHINESS OUTPUT ======="
 $CMD 2>&1
-} | ts -i " (%.S)]" | ts "[%H:%M:%.s" | sed -u -e "s/$/\r/g" |  tee -a "$LOGFILE"
+} | ts -i " (%.S)]" | ts "[%H:%M:%.S" |  tee -a "$LOGFILE" | sed -u -e "s/$/\r/g"
