7a9eeb1c5c2831bbb6f707d6185c2410d3e051e6
Tobias Frejo <tobias@frejo.dk>
2025-04-24 07:56:14 +0000

    timestamping twc output

--- maple-loops/HelloWorld/LOLA_specs/log_trustworthiness.sh
+++ maple-loops/HelloWorld/LOLA_specs/log_trustworthiness.sh
@@ -25,4 +25,4 @@ echo ""
 echo ""
 echo "======== TRUSTWORTHINESS OUTPUT ======="
 $CMD 2>&1
-} | tee -a "$LOGFILE"
+} | ts -i " (%.S)]" | ts "[%H:%M:%.s" | sed -u -e "s/$/\r/g" |  tee -a "$LOGFILE"
