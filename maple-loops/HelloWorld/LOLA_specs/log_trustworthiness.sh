3a965bd3f2c849e8f0f2e37475d1ef539d8de8ba
Tobias Frejo <tobias@frejo.dk>
2025-05-12 14:55:19 +0200

    use latest TWC

--- maple-loops/HelloWorld/LOLA_specs/log_trustworthiness.sh
+++ maple-loops/HelloWorld/LOLA_specs/log_trustworthiness.sh
@@ -9,7 +9,7 @@ LOGFILE="Logs/${SPECBASE}_$(date +%Y-%m-%d_%H.%M.%S).txt"
 IN_TOPICS="$(awk -F'[: ]+' '/^in/{for (i=2; i<=NF-1; i++) printf "%s ", $i}' $SPECFILE | sed 's/ *$//')"
 OUT_TOPICS="$(awk -F'[: ]+' '/^out/{for (i=2; i<=NF-1; i++) printf "%s ", $i}' $SPECFILE | sed 's/ *$//')"
 
-CMD="docker run --network host -it --rm -e RUST_BACKTRACE=full -v $HOSTDIR:/mnt/host_models trustworthiness-checker /mnt/host_models/$SPECBASE --input-mqtt-topics $IN_TOPICS"
+CMD="docker run --network host -it --rm -e RUST_BACKTRACE=full -v $HOSTDIR:/mnt/host_models localhost/trustworthiness-checker /mnt/host_models/$SPECBASE --input-mqtt-topics $IN_TOPICS"
 #  --output-mqtt-topics $OUT_TOPICS
 
 echo "Saving output to: $LOGFILE"
