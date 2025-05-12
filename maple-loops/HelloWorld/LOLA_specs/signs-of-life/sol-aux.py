dea9afdd4b1c158a7a22ac2617047ddc5a092462
Amalie Kaastrup-Hansen <amalie@kaastrup-hansen.dk>
2025-05-12 09:21:42 +0200

    Architectural property updates

--- maple-loops/HelloWorld/LOLA_specs/signs-of-life/sol-aux.py
+++ maple-loops/HelloWorld/LOLA_specs/signs-of-life/sol-aux.py
@@ -24,9 +24,7 @@ def on_unsubscribe(client, userdata, mid, reason_code_list, properties):
     client.disconnect()
 
 def on_message(client, userdata, message):
-    if message.topic == "/new_data":
-        client.publish('SOLClock', '{"Str": "monitor"}')
-    elif message.topic == "/Scan":
+    if message.topic == "/Scan":
         client.publish('SOLClock', '{"Str": "scan"}')
 
 def on_connect(client, userdata, flags, reason_code, properties):
